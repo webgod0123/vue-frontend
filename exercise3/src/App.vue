5136e03e72e1365b44857b5aa5ae989935f1d8cc
webgod0123 <miracletalent0123@gmail.com>
2022-10-21 05:44:26 -0400

    exercise 3

--- exercise3/src/App.vue
+++ exercise3/src/App.vue
@@ -1,47 +1,54 @@
 <template>
-
-  <h2>Exercise 3</h2>
-  <h4>Statement</h4>
-  <div class="statement">
-    <p>
-      You'll find a VueJS page running, missing CSS styles.
-    </p>
-    <p>
-      Without changing any HTML or Javascript, and using predefined CSS variables,
-      update the style of the page, so then it matches the designs and situations below:
-    </p>
-    <ul class="examples">
-      <li>Background layout
-        <img class="preview" src="./assets/backgroundLayout.png"/>
-      </li>
-      <li>Dynamic
-        <img class="preview" src="./assets/dynamic.gif"/>
-      </li>
-      <li>Responsive
-        <img class="preview" src="./assets/responsive.gif"/>
-      </li>
-    </ul>
-  </div>
-  <h4>Solution</h4>
-  <div class="solution">
-
-    <div class="input-container">
-      <span>Number of items to display: </span>
-      <input type="number" v-model="dataSize" min="1" :max="people.length"/>
+  <div>
+    <h2>Exercise 3</h2>
+    <h4>Statement</h4>
+    <div class="statement">
+      <p>
+        You'll find a VueJS page running, missing CSS styles.
+      </p>
+      <p>
+        Without changing any HTML or Javascript, and using predefined CSS
+        variables, update the style of the page, so then it matches the designs
+        and situations below:
+      </p>
+      <ul class="examples">
+        <li>
+          Background layout
+          <img class="preview" src="./assets/backgroundLayout.png" />
+        </li>
+        <li>
+          Dynamic
+          <img class="preview" src="./assets/dynamic.gif" />
+        </li>
+        <li>
+          Responsive
+          <img class="preview" src="./assets/responsive.gif" />
+        </li>
+      </ul>
     </div>
-    <div class="container">
-      <div class="item" v-for="person in items" :key="person.id">
-        <div class="name">{{ person.name }}</div>
-        <div class="age"><b>Age:</b> <span>{{ person.age }}</span></div>
-        <div class="eye-color">
-          <b class="eye">Eyes:</b>
-          <img src="./assets/eye.png" :class="[person.eyeColor, 'eye-icon']" />
+    <h4>Solution</h4>
+    <div class="solution">
+      <div class="input-container">
+        <span>Number of items to display: </span>
+        <input type="number" v-model="dataSize" min="1" :max="people.length" />
+      </div>
+      <div class="container">
+        <div class="item" v-for="person in items" :key="person.id">
+          <div class="name">{{ person.name }}</div>
+          <div class="age">
+            <b>Age:</b> <span>{{ person.age }}</span>
+          </div>
+          <div class="eye-color">
+            <b class="eye">Eyes:</b>
+            <img
+              src="./assets/eye.png"
+              :class="[person.eyeColor, 'eye-icon']"
+            />
+          </div>
         </div>
       </div>
     </div>
-
   </div>
-
 </template>
 
 <script>
@@ -65,56 +72,136 @@
 </script>
 
 <style lang="scss">
+.statement,
+.solution {
+  border: 1px solid #dedede;
+  padding: 0 0.5em;
+  min-height: 40px;
 
-  .statement, .solution {
-    border: 1px solid #dedede;
-    padding: 0 0.5em;
-    min-height: 40px;
+  .code {
+    color: #434343;
+    background-color: #f9f9f9;
+    padding: 0 5px;
+  }
 
-    .code {
-      color: #434343;
-      background-color: #f9f9f9;
-      padding: 0 5px;
-    }
+  .examples {
+    text-align: center;
+    list-style: none;
 
-    .examples {
-      text-align: center;
-      list-style: none;
+    li {
+      display: inline-block;
 
-      li {
-        display:inline-block;
-
-        .preview {
-          display: block;
-          margin: 5px;
-          height: 170px;
-          border: 1px solid black;
-        }
+      .preview {
+        display: block;
+        margin: 5px;
+        height: 170px;
+        border: 1px solid black;
       }
     }
+  }
 
-    .input-container {
-      text-align: center;
+  .input-container {
+    text-align: center;
+  }
+}
+
+:root {
+  --font: Avenir, Helvetica, Arial, sans-serif;
+  --background-1: #004c6d;
+  --background-2: #547c98;
+  --background-3: #93afc5;
+  --eye-brown: 192deg;
+  --eye-green: 290deg;
+  --eye-blue: -32deg;
+  --item-margin: 10px;
+  --item-padding: 20px;
+  --border-radius: 5px;
+
+  --items-by-row-when-screen-is-less-than-600px: 2;
+  --items-by-row-when-screen-is-more-than-600px: 4;
+  --items-by-row-when-screen-is-more-than-1024px: 7;
+}
+
+// TODO: Add your CSS Styling here
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.item {
+  box-sizing: border-box;
+  width: calc(
+    (
+        100% - 2 * var(--items-by-row-when-screen-is-more-than-1024px) *
+          var(--item-margin)
+      ) / var(--items-by-row-when-screen-is-more-than-1024px)
+  );
+  margin: var(--item-margin);
+  padding: var(--item-padding);
+  border-radius: var(--border-radius);
+  color: white;
+
+  &:nth-child(3n) {
+    background: var(--background-1);
+  }
+
+  &:nth-child(3n + 1) {
+    background: var(--background-2);
+  }
+
+  &:nth-child(3n + 2) {
+    background: var(--background-3);
+  }
+
+  .name {
+    font-size: 20px;
+  }
+
+  .age {
+    margin-top: 10px;
+    font-size: 20px;
+  }
+
+  .eye-color {
+    margin-top: 10px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+
+    .eye-icon {
+      width: 30px;
+      margin-left: 10px;
+
+      &.green {
+        filter: hue-rotate(var(--eye-green));
+      }
+      &.blue {
+        filter: hue-rotate(var(--eye-blue));
+      }
+      &.brown {
+        filter: hue-rotate(var(--eye-brown));
+      }
     }
   }
 
-  :root {
-    --font: Avenir, Helvetica, Arial, sans-serif;
-    --background-1: #004c6d;
-    --background-2: #547c98;
-    --background-3: #93afc5;
-    --eye-brown: 192deg;
-    --eye-green: 290deg;
-    --eye-blue: -32deg;
-    --item-margin: 10px;
-    --item-padding: 20px;
-    --border-radius: 5px;
-
-    --items-by-row-when-screen-is-less-than-600px: 2;
-    --items-by-row-when-screen-is-more-than-600px: 4;
-    --items-by-row-when-screen-is-more-than-1024px: 7;
+  @media (max-width: 1024px) {
+    width: calc(
+      (
+          100% - 2 * var(--items-by-row-when-screen-is-more-than-600px) *
+            var(--item-margin)
+        ) / var(--items-by-row-when-screen-is-more-than-600px)
+    );
   }
 
-  // TODO: Add your CSS Styling here
-
+  @media (max-width: 600px) {
+    width: calc(
+      (
+          100% - 2 * var(--items-by-row-when-screen-is-less-than-600px) *
+            var(--item-margin)
+        ) / var(--items-by-row-when-screen-is-less-than-600px)
+    );
+  }
+}
 </style>
